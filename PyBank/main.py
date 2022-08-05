b6a0e60ce9df6abf98cc5a9d0d33385a429d45cf
unknown <dvdmasnk@gmail.com>
2022-08-04 21:36:24 -0500

    added average change

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,36 +1,41 @@
+from calendar import month
 import os
 import csv
 
-bankpath = os.path.join("PyBank", "Resources", "budget_data.csv")
+bankpath = 'python-challenge/PyBank/Resources/budget_data.csv'
 #a la was with me and we tried multiple different pathway functions which wouldn't work except for this
 
 c = 0
 total_months = 0
 total_net = 0
 net_change = 0
-current_month = 0
-previous_month = 0
-profit_loss_ot = []
-date = []
+net_change_list = []
+month_change = []
 
 with open(bankpath, 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     next(csvreader)
+    first_row = next(csvreader)
     for rows in csvreader:
-        if c ==1:
-            previous_month = current_month
+         #total months
+        total_months += 1
+        #total net profit/loss
+        total_net += int(rows[1])
+        #changes in profit/loss and average of change
+        #first_row = int(rows[1])
+        if first_row[1] == rows[1]:
+            net_change_list.append(rows[1])
+            month_change.append(rows[0])
         else:
-            #total months
-            total_months += 1
-            #total net profit/loss
-            total_net += int(rows[1])
-            #changes in profit/loss and average of change
-            net_change = current_month - previous_month
-            profit_loss_ot.append(net_change)
-            previous_month = current_month
+            current_row = int(rows[1])
+            net_change = current_row - (current_row-1)
+            net_change_list.append(rows[1])
+            month_change.append(rows[0])
+    
+
 
 
             
     print(total_months)
     print(total_net)
-    print(net_change)
+    print(net_change_list)
